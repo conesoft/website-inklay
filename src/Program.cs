50f41e286c195879491c54d491b5b043f4dc1e9d
David Spörri <davepermen@hotmail.com>
2025-02-04 23:51:13 +0100

    yo

--- src/Program.cs
+++ src/Program.cs
@@ -1,43 +1,34 @@
 using Conesoft.Hosting;
+using Conesoft.PwaGenerator;
+using Conesoft.Website.Inklay.Components;
 using Conesoft.Website.Inklay.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddHostConfigurationFiles(configurator =>
-{
-    configurator.Add<CalendarCache.Settings>();
-});
-builder.AddHostEnvironmentInfo();
-builder.AddLoggingService();
+builder
+    .AddHostConfigurationFiles(configurator =>
+    {
+        configurator.Add<CalendarCache.Settings>();
+    })
+    .AddHostEnvironmentInfo()
+    .AddLoggingService()
+    .AddUsersWithStorage()
+    ;
 
-// Add services to the container.
 builder.Services
     .AddCompiledHashCacheBuster()
     .AddHttpClient()
     .AddHostedServiceWith<CalendarCache>(TimeSpan.FromMinutes(5))
     .AddHostedServiceWith<WeatherCache>(TimeSpan.FromMinutes(30))
-    .AddRazorComponents();
+    .AddRazorComponents()
+    ;
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseLoggingServiceOnRequests();
-
-app
-    .UseCompiledHashCacheBuster()
-    .UseDeveloperExceptionPage()
-    .UseHttpsRedirection()
-    .UseAntiforgery();
-
+app.MapPwaInformationFromAppSettings();
+app.MapUsersWithStorage();
 app.MapStaticAssets();
-app.MapRazorComponents<Conesoft.Website.Inklay.Components.App>();
+app.MapRazorComponents<App>();
 
 app.Run();
 
